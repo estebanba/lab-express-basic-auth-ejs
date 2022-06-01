4fc52700d620d01a544512af04c304a904fe17d2
estebanba <esteban.ba@gmail.com>
2022-06-01 17:47:22 +0200

    Iteration 2 Login part done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,13 +17,41 @@ router.post("/signup", async (req, res) => {
         const salt = await bcryptjs.genSalt(saltRounds);
         const hashedPassword = await bcryptjs.hash(password, salt);
         await User.create({ username, password: hashedPassword });
-        res.redirect("/userProfile");
+        res.render("auth/userProfile", {username});
     } catch (error) {
         console.log("error", error)
     }        
 })
 
-router.get("/userProfile", (req, res) => res.render("auth/userProfile"))
+///LOGIN////
+router.get("/login", (req, res) => res.render("auth/login"))
 
+router.post("/login", async (req, res, next) => {
+    const { username, password } = req.body;
+
+    if (username === "" || password === "") {
+
+        res.render("auth/login", {
+            errorMessage: "Please enther both username and password to login."
+        });
+        return;
+    }
+
+    const userFromDB = await User.findOne({ username });
+    if (!userFromDB) {
+        
+        return;
+    }
+
+    if (bcryptjs.compareSync(password, userFromDB.password)) {
+        console.log(userFromDB);
+        res.render("auth/userProfile", userFromDB);
+    } else {
+        res.render("auth/login", {
+            errorMessage: "Wrong password, biatch!"
+        });
+    }
+})
 
 module.exports = router
+
